6c6b52037be6a0197ee6be5997c969825a8b95ac
fanbt1 <fanbt1@lenovo.com>
2020-07-28 15:36:15 +0800

    add service project

---
+++ TangerineService/ProjectInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TangerineService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
